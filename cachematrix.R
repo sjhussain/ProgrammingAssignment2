53f2dcf6ed5f12e3ddb88b9713a0e7cb8e6af5e0
Syed Jawwad Hussain <sjawwadh@gmail.com>
2016-03-19 15:19:03 +0300

    Initial fork and add

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Name: Syed Jawwad Hussain
+## Project Description:
+##
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
