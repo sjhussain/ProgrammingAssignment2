6488b504eba8cc02c26ace7e7c48552ef39a6803
Syed Jawwad Hussain <sjawwadh@gmail.com>
2016-03-19 22:34:09 +0300

    Implemented both functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+#################################################################################
+##
 ## Name: Syed Jawwad Hussain
 ## Project Description:
+##      The purpose of this Project is to ease the computation of the matrix by
+##      create two functions in R language to use the caching feature to
+##      increase the performance of applying inverse functionality to a matrix
 ##
+################################################################################
 
-## Write a short comment describing this function
+#################################################################################
+##
+## Function Name: makeCacheMatrix
+## Function Input: matrix
+## Function Output: matrix
+## Function Purpose:  Create a Matrix which is cached so that 
+##                    it is created only on demand using the set and get function
+##                    the solve function is used to inverse the matrix
+##
+#################################################################################
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setm <- function(solve) m <<- solve
+  getm <- function() m
+  list(set = set, get = get,
+       setm = setm,
+       getm = getm)
 }
 
 
-## Write a short comment describing this function
+#################################################################################
+##
+## Function Name:     cacheSolve
+## Function Input:    matrix
+## Function Output:   matrix
+## Function Purpose:  The cache function will create the matrix and then call
+##                    the solve function to inverse the matrix
+##
+#################################################################################
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+  m <- x$getm()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setm(m)
+  m
 }
